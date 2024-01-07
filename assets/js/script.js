2bbce50775c11f9cd701d7d6866a041aa818f1fe
Laura Kane <148717413+kauralane@users.noreply.github.com>
2024-01-07 09:23:52 +0000

    fixed colour loading issue.

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,15 +12,15 @@ function showColours() {
 let hours = [9, 10, 11, 12, 13, 14, 15, 16]
 for (let i = 0; i < hours.length; i++) {
     let scheduleTime = dayjs().hour(hours[i]);
-    // let row = $(`#${i + 9}-row`);
-    let row = $('.row').children('.user-input')
+    let row = $(`#${i + 9}-row`)
+    let colourArea = row.find('.user-input')
 
     if (scheduleTime.isSame(time, 'hour')) {
-        row.addClass('present');
+        colourArea.addClass('present');
     } else if (scheduleTime.isAfter(time, 'hour')) {
-        row.addClass('future');
+        colourArea.addClass('future');
     } else {
-        row.addClass('past');
+        colourArea.addClass('past');
     }
 }}
 
